9971df75d790c0fd931ac124ca3f469d49bc9301
Moberaigner <oberaigner13@gmail.com>
2018-10-07 19:02:55 +0200

    Abgabe

--- project01/src/main/java/at/htl/webscraper/Main.java
+++ project01/src/main/java/at/htl/webscraper/Main.java
@@ -11,27 +11,44 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static ScheduledExecutorService exec =
+            Executors.newSingleThreadScheduledExecutor();
+    private static String link;
+
     public static void main(String[] args) {
         try {
-            HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+            //create and start Webserver
+            HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0); //runs on port 8000
             server.createContext("/", new MyHandler());
             server.setExecutor(null);
             server.start();
+
+            //update link and write to file every 60 seconds
+            exec.scheduleAtFixedRate(() -> {
+                link = update();
+                writeToFile(link);
+            }, 0, 60, TimeUnit.SECONDS);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static String output() {
+
+    public static String update() {
+        //get actual link for the video
         Document doc;
         String output = "";
         try {
@@ -44,14 +61,27 @@ public class Main {
         return output;
     }
 
-    static class MyHandler implements HttpHandler {
+    public static void writeToFile(String text) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("myFile.txt",true));
+            writer.append("\n");
+            writer.append(text + "     "+LocalDateTime.now());
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    static class MyHandler implements HttpHandler {  //Webserver
         @Override
         public void handle(HttpExchange t) throws IOException {
             String response = "<video controls>\n" +
-                    "  <source src=\"" + output() + "\" type=\"video/mp4\">\n" +
+                    "  <source src=\"" + link + "\" type=\"video/mp4\">\n" +
                     "</video>";
             Headers h = t.getResponseHeaders();
-            h.set("Content-Type","text/html");
+            h.set("Content-Type", "text/html");      //send as html text
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
--- project02/app/src/main/java/at/htl/webscraper/MainActivity.kt
+++ project02/app/src/main/java/at/htl/webscraper/MainActivity.kt
@@ -1,11 +1,15 @@
 package at.htl.webscraper
 
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
 import android.widget.VideoView
-
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.io.IOException
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import android.os.StrictMode
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +17,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val uri = Uri.parse("http://172.18.3.169:8000")
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+        }
         val video = findViewById(R.id.videoView) as VideoView
-        video.setVideoURI(uri)
-        video.start()
+
+        val t = object : Thread() {
+            override fun run() {
+                try {
+                    while (!isInterrupted) {
+                        runOnUiThread {
+                            video.setVideoPath(update())
+                            video.start()
+                        }
+                        Thread.sleep(60000) // wait 60s
+                    }
+                } catch (e: InterruptedException) {
+                }
+
+            }
+        }
+        t.start()
     }
+
+    fun update(): String {
+        //get actual link for the video
+        val doc: Document
+        var output = ""
+        try {
+            doc = Jsoup.connect("https://webtv.feratel.com/webtv/?cam=5132&design=v3&c0=0&c2=1&lg=en&s=0").get()
+            output = doc.getElementById("fer_video").select("source").attr("src")
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return output
+    }
+
 }
